552da4421d8294045ee146fca8c41d085b904af5
champix56 <desorbaix@free.fr>
2022-04-13 13:39:06 +0100

    auto format

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,8 +1,8 @@
-import React from 'react'
-import './Button.css'
-const Button = (props)=>{
-    console.log(props);
-    return <button className="Button">{props.text}</button>;
-}
+import React from "react";
+import "./Button.css";
+const Button = (props) => {
+  console.log(props);
+  return <button className="Button">{props.text}</button>;
+};
 
 export default Button;
