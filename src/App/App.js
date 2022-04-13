15a301532fa62ea1520dca19239b9f95bf024e47
champix56 <desorbaix@free.fr>
2022-04-13 13:28:07 +0100

    usage des props + usage js dans xml

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import Button from './components/Button/Button'
-import './App.css';
+import React from "react";
+import Button from "./components/Button/Button";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       DEMAT BREIZH
-      <br/>
-      <Button/>
+      <hr />
+      <Button text="Hello" />
+      <Button text="de lu" />
+      <Button text="benjamin button" />
+      <Button />
+
     </div>
   );
 }
