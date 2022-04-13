7c1e867deec0c3f93689ad2a5d54db96e3e935d1
champix56 <desorbaix@free.fr>
2022-04-13 13:28:29 +0100

    usage props dans cmp + usage js dans cmp xml

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './Button.css'
 const Button = (props)=>{
     console.log(props);
-    return <button className="Button">Demat</button>;
+    return <button className="Button">{props.text}</button>;
 }
 
 export default Button;
