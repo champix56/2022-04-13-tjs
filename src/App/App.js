54387185b26b697996b0fb4bd014b3b65f0c98da
champix56 <desorbaix@free.fr>
2022-04-13 11:25:10 +0100

    mise en oeuvre du composant

--- src/App/App.js
+++ src/App/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
+import Button from './components/Button/Button'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       DEMAT BREIZH
+      <br/>
+      <Button/>
     </div>
   );
 }
