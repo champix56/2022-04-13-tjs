98ed60f787e3f439e32aaf278bd89463ecfea7fd
champix56 <desorbaix@free.fr>
2022-04-14 22:08:39 +0200

    ajout de liens react router-dom + react-bootstrap compatible

--- src/App/components/ui/Navbar/Navbar.js
+++ src/App/components/ui/Navbar/Navbar.js
@@ -2,17 +2,19 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./Navbar.module.css";
 import {Navbar as NavBar,Container, Nav} from 'react-bootstrap'
+import {LinkContainer } from 'react-router-bootstrap'
 import '../../../../../node_modules/bootstrap/dist/css/bootstrap.css'
 const Navbar = (props) => {
   return (
     <div className={styles.Navbar} data-testid="Navbar">
       <NavBar bg="primary" variant="dark">
         <Container>
-          <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+          <LinkContainer to="/"><NavBar.Brand>Meme.js</NavBar.Brand></LinkContainer>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <LinkContainer to="/"><Nav.Link >Home</Nav.Link></LinkContainer>
+            <LinkContainer to="/thumbnail"><Nav.Link >Thumbnail</Nav.Link></LinkContainer>
+            <LinkContainer to="/editor"><Nav.Link >New</Nav.Link></LinkContainer>
+            <LinkContainer to="/editor/0"><Nav.Link >Edit id:0</Nav.Link></LinkContainer>
           </Nav>
         </Container>
       </NavBar>
