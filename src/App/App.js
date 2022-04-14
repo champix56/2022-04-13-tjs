66867dabfacd5727bba7cab42608145c1aef4081
champix56 <desorbaix@free.fr>
2022-04-14 13:33:35 +0100

    add navbar base from react-bootstrap

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
 import FlexHLayout from "./components/layout/FlexHLayout/FlexHLayout";
 import FlexWLayout from "./components/layout/FlexWLayout/FlexWLayout";
+import Navbar from './components/ui/Navbar/Navbar'
 import './App.css'
 class App extends Component {
   render() {
     return (
       <div className="App" style={{height:'95vh'}}>
         <FlexHLayout>
-          <div className="navbar">top</div>
+          <Navbar/>
           <FlexWLayout>
             <div className="left">left</div>
             <div className="right">right</div>
---
+++ src/App/components/ui/Navbar/Navbar.js
@@ -0,0 +1,23 @@
+import React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import styles from "./Navbar.module.css";
+import {Navbar as NavBar,Container, Nav} from 'react-bootstrap'
+import '../../../../../node_modules/bootstrap/dist/css/bootstrap.css'
+const Navbar = (props) => {
+  return (
+    <div className={styles.Navbar} data-testid="Navbar">
+      <NavBar bg="primary" variant="dark">
+        <Container>
+          <NavBar.Brand href="#home">Navbar</NavBar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </NavBar>
+    </div>
+  );
+};
+
+export default Navbar;
