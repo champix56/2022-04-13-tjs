623864cf33b050db93d8bbf105418dffaf3b7487
champix56 <desorbaix@free.fr>
2022-04-14 10:08:21 +0100

    gestion d'etat sans cycle de vie

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from 'prop-types'
 import styles from "./Button.module.css";
+
 const Button = (props) => {
-  console.log(props);
+  //console.log(props);
+  const [isClicked, setIsClicked] = useState(false);
+ 
   return (
     <button
-      className={styles.Button}
+      className={!isClicked? styles.Button:styles.Button +' '+styles.clicked}
       style={{ backgroundColor: props.bgColor, color: props.color, ...props.style}}
       onClick={(evt) => {
+        setIsClicked(true)
+        
+        //un etat de retard du a l'async de setIsClicked
+        console.log(isClicked);
+
         props.onButtonClicked("une valeur ou pas de valeur au choix");
       }}
       type={props.type}
     >
-      {props.children}
+      {props.children}<br/>{isClicked.toString()}
     </button>
   );
 };
