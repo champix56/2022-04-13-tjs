7b48834c17133b86b542014243f73d2819108ce9
champix56 <desorbaix@free.fr>
2022-04-14 08:28:50 +0100

    passage en class d'un cmp

--- src/App/App.js
+++ src/App/App.js
@@ -2,28 +2,35 @@ import React from "react";
 import Button from "./components/Button/Button";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      DEMAT BREIZH
-      <hr />
-      <Button
-        onButtonClicked={(unParam) => {
-          console.log(unParam);
-        }}
-        bgColor="tomato"
-      >
-        <img src="/img/ok_check.png" />
-        <div style={{ display: "inline" }}>Mon text</div>
-      </Button>
-      <Button bgColor="MidnightBlue">de lu</Button>
-      <Button bgColor="grey" color="MidnightBlue">benjamin button</Button>
-      <Button color="MidnightBlue">
-        <img src="/img/ok_check.png" />
-      </Button>
-      <Button style={{textDecoration:'underline',fontSize:'48pt',color:'yellow'}}>Def. button</Button>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <div className="App">
+        counter = {counter}
+        <hr />
+        <Button
+          onButtonClicked={(unParam) => {
+            counter++;
+            console.log(counter);
+          }}
+          bgColor="tomato"
+        >
+          <img src="/img/ok_check.png" />
+          <div style={{ display: "inline" }}>Mon text</div>
+        </Button>
+      </div>
+    );
+  }
 }
 
+/*
+function App() {
+  return (
+   //equiv du render de la class
+  );
+}
+*/
 export default App;
