44061c1e384ac23d1c0cb34b59fcc0c7ac178f15
champix56 <desorbaix@free.fr>
2022-04-14 21:17:33 +0200

    creation d'enum exportable pour les actions du reducer ressource

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -3,11 +3,15 @@ import { createStore } from "redux";
 const initialState = {
   images: [],
 };
+export const ACTIONS_RESSOURCES = Object.freeze({
+  ADD_IMAGES: "ADD_IMAGES",
+  ADD_IMAGE: "ADD_IMAGE",
+});
 function ressourcesReducer(state = initialState, action) {
   switch (action.type) {
-    case "ADD_IMAGES":
+    case ACTIONS_RESSOURCES.ADD_IMAGES:
       return { ...state, images: action.values };
-    case "ADD_IMAGE":
+    case ACTIONS_RESSOURCES.ADD_IMAGE:
       return { ...state, images: [...state.images, action.value] };
     default:
       return state;
