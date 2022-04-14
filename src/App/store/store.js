a65adaa10ba4949b2e227551dab9e0b5c708a90a
champix56 <desorbaix@free.fr>
2022-04-14 21:15:34 +0200

    clean du store.js

--- src/App/store/store.js
+++ src/App/store/store.js
@@ -13,63 +13,7 @@ function ressourcesReducer(state = initialState, action) {
       return state;
   }
 }
-// let state = ressourcesReducer(undefined, {
-//   type: "ADD_IMAGES",
-//   values: [
-//     {
-//       id: 0,
-//       url: "futurama_slurm.jpg",
-//       w: 600,
-//       h: 315,
-//       name: "futurama slurm",
-//     },
-//     {
-//       id: 1,
-//       url: "futurama_matrix.webp",
-//       w: 1280,
-//       h: 720,
-//       name: "futurama matrix",
-//     },
-//   ],
-// });
-
-// state=ressourcesReducer(state,{
-//     type:'ADD_IMAGE',
-//     value:{
-//         id:123,
-//         url:'hello.jsph'
-//     }
-// });
-
 export const store = createStore(ressourcesReducer);
 store.subscribe(() => {
   console.log(store.getState());
-});
-
-store.dispatch({
-  type: "ADD_IMAGES",
-  values: [
-    {
-      id: 0,
-      url: "futurama_slurm.jpg",
-      w: 600,
-      h: 315,
-      name: "futurama slurm",
-    },
-    {
-      id: 1,
-      url: "futurama_matrix.webp",
-      w: 1280,
-      h: 720,
-      name: "futurama matrix",
-    },
-  ],
-});
-
-store.dispatch({
-  type: "ADD_IMAGE",
-  value: {
-    id: 123,
-    url: "hello.jsph",
-  },
 });
