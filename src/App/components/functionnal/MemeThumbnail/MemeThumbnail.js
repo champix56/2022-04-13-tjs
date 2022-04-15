4f19320fd66d6824f965b54ac6d5013160dab134
champix56 <desorbaix@free.fr>
2022-04-15 10:07:20 +0100

    useSelectore

--- src/App/components/functionnal/MemeThumbnail/MemeThumbnail.js
+++ src/App/components/functionnal/MemeThumbnail/MemeThumbnail.js
@@ -4,25 +4,28 @@ import styles from "./MemeThumbnail.module.css";
 import ThumbnailLayout from "../../layout/ThumbnailLayout/ThumbnailLayout";
 import MemeViewer from "../../ui/MemeViewer/MemeViewer";
 import { Link } from "react-router-dom";
-
-const MemeThumbnail = (props) => (
-  <div className={styles.MemeThumbnail} data-testid="MemeThumbnail">
-    <ThumbnailLayout>
-      {props.memes.map((e, i) => (
-        <Link to={`/editor/${e.id}`} key={i}>
-          <MemeViewer
-            meme={e}
-            image={props.images.find((img) => img.id === e.imageId)}
-          />
-        </Link>
-      ))}
-    </ThumbnailLayout>
-  </div>
-);
-
+import { useSelector } from "react-redux";
+const MemeThumbnail = (props) => {
+  const ressources = useSelector(state => state.ressources)
+  return (
+    <div className={styles.MemeThumbnail} data-testid="MemeThumbnail">
+      <ThumbnailLayout>
+        {ressources.memes.map((e, i) => (
+          <Link to={`/editor/${e.id}`} key={i}>
+            <MemeViewer
+              meme={e}
+              image={ressources.images.find((img) => img.id === e.imageId)}
+            />
+          </Link>
+        ))}
+      </ThumbnailLayout>
+    </div>
+  );
+};
+/*
 MemeThumbnail.propTypes = {
   memes: PropTypes.array.isRequired,
   images: PropTypes.array.isRequired,
-};
+};*/
 
 export default MemeThumbnail;
