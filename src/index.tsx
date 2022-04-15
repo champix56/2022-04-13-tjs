f4f633d5edea85a5c8770df21403739075e30383
champix56 <desorbaix@free.fr>
2022-04-15 09:54:24 +0100

    context de store obligatoir pour react-redux

--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,20 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App/App";
+import { Provider } from "react-redux";
 //import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from "react-router-dom";
+import { store } from "./App/store/store";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>
 );
 
